b0b8326526b5d0b430cd6b1ade67a890ee95179b
alestucci <alessandrotucci@gmail.com>
2022-02-25 15:24:18 +0100

    Root files

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+Snack 2
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+Snack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+(provatelo anche senza array)
+Snack 4
+In un array sono contenuti i nomi degli invitati alla festa del grande
+Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+no alla festa.
+Snack 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+Snack 6
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+*/
